ef5221c964a60ea8b85a2b821c670b0104b06fdc
hzwedt@gmail.com <hzwedt@gmail.com>
2024-01-02 14:01:58 +0100

    tr for render

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// Add these functions to handle the modal
+
 function openModal() {
   const modal = document.getElementById('aboutModal');
   modal.style.display = 'block';
@@ -9,7 +9,4 @@ function closeModal() {
   modal.style.display = 'none';
 }
 
-// ... your existing JavaScript ...
-
-// Add a listener to the "About Us" menu item
 document.querySelector('a[href="#AboutUs"]').addEventListener('click', openModal);
