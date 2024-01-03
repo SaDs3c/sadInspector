306780978ca19db90820dc7885a26da0ff8c5fbd
hzwedt@gmail.com <hzwedt@gmail.com>
2024-01-03 16:15:37 +0100

    view source

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,13 +14,17 @@ const view_source = document.querySelector("[data-view-source]")
 const about_us = document.querySelector("[data-about-us]")
 const web_view = document.querySelector("[data-web-view]");
 
+// let <pre id="sourceCode"></pre>
 
 // Event Listeners
 attack_btn.addEventListener("click", () => {
   if (url_input.value.trim() === '') {
+    web_view.setAttribute("src", '');
     alert('No input given');
+    
   } else {
     web_view.setAttribute("src", url_input.value);
+    url_input.setAttribute("value", web_view.src);
   }
 });
 extract_url.addEventListener("click", () => {
@@ -28,11 +32,17 @@ extract_url.addEventListener("click", () => {
 });
 view_source.addEventListener("click", () => {
   alert("View Source Code");
+  web_view.setAttribute("src", "view-source:"+url_input.value);
+  url_input.setAttribute("value", web_view.src);
 });
 toolkit_btn.addEventListener("click", () => {
   alert("Shield 🛡️");
 });
 about_us.addEventListener("click", openModal);
+web_view.contentWindow.addEventListener("click", () => {
+  alert(web_view.contentWindow.location.href);
+  url_input.value =  web_view.contentWindow.location.href;
+});
 
 
 
@@ -45,6 +55,13 @@ function closeModal() {
   const modal = document.getElementById('aboutModal');
   modal.style.display = 'none';
 }
-
+function viewSource() {
+  if (urlFrame.contentDocument) {
+    // Display the source code in the pre element
+    sourceCodeElement.textContent = urlFrame.contentDocument.documentElement.outerHTML;
+  } else {
+    alert('Iframe content not loaded.');
+  }
+}
 
 console.log('Script conclude execution here.');
